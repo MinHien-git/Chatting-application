a314cbd1f5e32b63a975650b7f1e1391c771661f
21127154 <=>
2023-11-29 23:48:37 +0700

    change to mvp architecture

---
+++ src/data/model/userModel.java
@@ -0,0 +1,80 @@
+package data.model;
+
+import data.repository.userRepo;
+
+import java.util.Date;
+
+public class userModel {
+    private String username;
+    private String fullname;
+    private String address;
+    private Date birthday;
+    private boolean gender;
+    private String email;
+
+    userModel(String username, boolean gender, String email) {
+        this.username = username;
+        this.gender = gender;
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getFullname() {
+        return this.fullname;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Date getBirthday() {
+        return this.birthday;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public boolean getGender() {
+        return this.gender;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String authenticate(String username, String password) {
+        userRepo checkLogin = new userRepo();
+        if (checkLogin.checkLogin(username, password)) {
+            return "in";
+        }
+        else {
+            return "out";
+        }
+    }
+
+    public boolean register(String username, String password, )
+}
---
+++ src/data/repository/userRepo.java
@@ -0,0 +1,11 @@
+package data.repository;
+
+public class userRepo {
+    public userRepo() {
+        System.out.println("hello");
+    }
+    public boolean checkLogin(String username, String password) {
+
+        return false;
+    }
+}
